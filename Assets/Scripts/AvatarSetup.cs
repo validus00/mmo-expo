61909ffb6a34964efb6356bbb87f50bc80ca858a
Jane Fong <janeefongg@gmail.com>
2021-01-19 14:26:06 -0800

    Remove reference from avatar list in UserInfo.cs

---
+++ Assets/Scripts/AvatarSetup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class AvatarSetup : MonoBehaviour {
+
+    private PhotonView __photonView;
+    public int avatarValue;
+    public GameObject myAvatar;
+    // Start is called before the first frame update
+    void Start() {
+        __photonView = GetComponent<PhotonView>();
+        if (__photonView.IsMine) {
+            __photonView.RPC("addCharacterRPC", RpcTarget.AllBuffered, UserInfo.info.mySelectedAvatar);
+        }
+    }
+
+    [PunRPC]
+    void addCharacterRPC(int avatar) {
+        avatarValue = avatar;
+    }
+}
---
+++ Assets/Scripts/UserInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInfo : MonoBehaviour {
+    public static UserInfo info;
+    public int mySelectedAvatar;
+
+    private void OnEnable() {
+        if (UserInfo.info == null) {
+            UserInfo.info = this;
+        } else {
+            if (UserInfo.info != this) {
+                Destroy(UserInfo.info.gameObject);
+                UserInfo.info = this;
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start() {
+        if (PlayerPrefs.HasKey("myAvatar")) {
+            mySelectedAvatar = PlayerPrefs.GetInt("myAvatar");
+        } else {
+            mySelectedAvatar = 0;
+            PlayerPrefs.SetInt("myAvatar", mySelectedAvatar);
+        }
+    }
+}
