fde3675c5af14bf1502ea3b00cd121c392dc5cfa
Kevin Wu <wukev@oregonstate.edu>
2021-02-08 15:01:01 -0800

    Added InfoBooth prefab and InfoBooth GameObjects to MainEventScene.

---
+++ Assets/Scripts/InfoBoothSetup.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class InfoBoothSetup : MonoBehaviourPunCallbacks {
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
