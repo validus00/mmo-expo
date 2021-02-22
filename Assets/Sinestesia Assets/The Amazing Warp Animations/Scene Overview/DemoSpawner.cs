b909c82a73abb72cf21f85b2f4861e403349d033
Kevin Wu <wukev@oregonstate.edu>
2021-02-22 15:53:51 -0800

    Added Sinestesia Assets (portal effects) package and updated Portal prefab. Deleted Doors asset package. Updated Booth and InfoBooth prefabs: rearranged the Canvas and added FaceCamera script.

---
+++ Assets/Sinestesia Assets/The Amazing Warp Animations/Scene Overview/DemoSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SSWarpAnimations
+{
+    public class DemoSpawner : MonoBehaviour {
+    	public GameObject prefab_to_spawn;
+        public float loop_time = 15f;
+    	private GameObject warp;
+
+    	// Use this for initialization
+    	void Start () {
+    		StartCoroutine("WaitAndSpawn");
+    	}
+    	
+
+    //warp = Instantiate(prefab_to_spawn, transform.position, transform.rotation);
+    	private IEnumerator WaitAndSpawn()
+        {
+        	float aux_random_time = Random.Range( 0.0f, 1.5f );
+        	yield return new WaitForSeconds(aux_random_time);
+
+            while (true)
+            {
+            	warp = Instantiate(prefab_to_spawn, transform.position, transform.rotation);
+                yield return new WaitForSeconds( loop_time );
+                Destroy(warp);            
+            }
+        }
+
+    }
+}
